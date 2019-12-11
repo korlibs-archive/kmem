abfafddbd0f33cb94285c95333cb79e507ae4c58
soywiz <soywiz@gmail.com>
2019-12-11 09:06:08 +0100

    Precompute empty addresses + simplify documentation

--- kmem/src/nativeCommonMain/kotlin/com/soywiz/kmem/MemExt.kt
+++ kmem/src/nativeCommonMain/kotlin/com/soywiz/kmem/MemExt.kt
@@ -3,15 +3,15 @@ package com.soywiz.kmem
 import kotlinx.cinterop.*
 
 //private val emptyPinnedBoolean = BooleanArray(1).pin()
-private val emptyPinnedByte = ByteArray(1).pin()
-private val emptyPinnedShort = ShortArray(1).pin()
-private val emptyPinnedInt = IntArray(1).pin()
-private val emptyPinnedLong = LongArray(1).pin()
+private val emptyAddressByte = ByteArray(1).pin().addressOf(0)
+private val emptyAddressShort = ShortArray(1).pin().addressOf(0)
+private val emptyAddressInt = IntArray(1).pin().addressOf(0)
+private val emptyAddressLong = LongArray(1).pin().addressOf(0)
 
-private val emptyPinnedUByte = UByteArray(1).pin()
-private val emptyPinnedUShort = UShortArray(1).pin()
-private val emptyPinnedUInt = UIntArray(1).pin()
-private val emptyPinnedULong = ULongArray(1).pin()
+private val emptyAddressUByte = UByteArray(1).pin().addressOf(0)
+private val emptyAddressUShort = UShortArray(1).pin().addressOf(0)
+private val emptyAddressUInt = UIntArray(1).pin().addressOf(0)
+private val emptyAddressULong = ULongArray(1).pin().addressOf(0)
 
 // Missing CharArray? (equivalent to UShort) and BooleanArray (equivalent to Byte)
 //val Pinned<BooleanArray>.startAddressOf: CPointer<BooleanVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedBoolean.addressOf(0)
@@ -19,19 +19,19 @@ private val emptyPinnedULong = ULongArray(1).pin()
 
 // @TODO: Performance penalty due to requiring calling `this.get()` to get the size of the array. Optimal solution should be provided my Kotlin/Native.
 
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<ByteArray>.startAddressOf: CPointer<ByteVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedByte.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<ShortArray>.startAddressOf: CPointer<ShortVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedShort.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<IntArray>.startAddressOf: CPointer<IntVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedInt.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<LongArray>.startAddressOf: CPointer<LongVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedLong.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<UByteArray>.startAddressOf: CPointer<UByteVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedUByte.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<UShortArray>.startAddressOf: CPointer<UShortVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedUShort.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<UIntArray>.startAddressOf: CPointer<UIntVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedUInt.addressOf(0)
-/** Returns a valid not-null address, either from the beginning of this array or from another address if the array is empty */
-val Pinned<ULongArray>.startAddressOf: CPointer<ULongVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyPinnedULong.addressOf(0)
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<ByteArray>.startAddressOf: CPointer<ByteVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressByte
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<ShortArray>.startAddressOf: CPointer<ShortVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressShort
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<IntArray>.startAddressOf: CPointer<IntVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressInt
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<LongArray>.startAddressOf: CPointer<LongVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressLong
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<UByteArray>.startAddressOf: CPointer<UByteVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressUByte
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<UShortArray>.startAddressOf: CPointer<UShortVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressUShort
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<UIntArray>.startAddressOf: CPointer<UIntVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressUInt
+/** Returns the address of the beginning of this array or if it is empty another valid non-null address */
+val Pinned<ULongArray>.startAddressOf: CPointer<ULongVar> get() = if (this.get().isNotEmpty()) this.addressOf(0) else emptyAddressULong
